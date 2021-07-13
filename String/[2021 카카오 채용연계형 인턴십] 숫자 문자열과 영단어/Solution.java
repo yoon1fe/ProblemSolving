539e4ebc85bd6f65d54021d276ec788e5e7fce0a
윤원철 <yoon1fe@n>
2021-07-13 23:01:00 +0900

    210713

---
+++ String/[2021 카카오 채용연계형 인턴십] 숫자 문자열과 영단어/Solution.java
@@ -0,0 +1,25 @@
+class Solution {
+  public int solution(String s) {
+    s = s.replaceAll("zero", "0");
+    s = s.replaceAll("one", "1");
+    s = s.replaceAll("two", "2");
+    s = s.replaceAll("three", "3");
+    s = s.replaceAll("four", "4");
+    s = s.replaceAll("five", "5");
+    s = s.replaceAll("six", "6");
+    s = s.replaceAll("seven", "7");
+    s = s.replaceAll("eight", "8");
+    s = s.replaceAll("nine", "9");
+
+    return Integer.parseInt(s);
+  }
+
+  public static void main(String[] args) {
+    Solution s = new Solution();
+    System.out.println(s.solution("one4seveneight"));
+    System.out.println(s.solution("23four5six7"));
+    System.out.println(s.solution("2three45sixseven"));
+    System.out.println(s.solution("123"));
+    System.out.println(s.solution("456779sixsixnine"));
+  }
+}
